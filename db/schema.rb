832f2495ceff532623bb96d8674f1d83b5d0bce5
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-07-23 13:12:03 -0400

    Added First and Last name to Users Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_23_132443) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_23_171057) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -42,6 +42,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_23_132443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
