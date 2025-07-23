a60828a7350701822e935e63218fdbcdf662505f
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-07-23 13:22:58 -0400

    Set up devise sign-up to include first and last name when when creating an account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   include Pundit::Authorization
 
   after_action :maybe_verify_authorized, unless: :skip_pundit?
   after_action :maybe_verify_policy_scoped, unless: :skip_pundit?
+
+  protected
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 
   private
 
