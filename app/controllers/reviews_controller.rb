bc2fdb711875e09c0827e732fbcd4f669fc5b9ad
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-07-24 03:16:37 -0400

    Users Authorize to create reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,14 +2,19 @@ class ReviewsController < ApplicationController
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
+    authorize @review
   end
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    authorize @review
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
